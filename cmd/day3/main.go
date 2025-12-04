c595c1705c06a142361749935061625aea1d2477
Douglas Moore <dmoore@justworks.com>
2025-12-03 20:15:39 -0500

    Start of day 3

---
+++ cmd/day3/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	filePath := "sample.txt"
+	if args[0] == "real" {
+		filePath = "lib/day3.txt"
+
+	}
+
+	fileArr, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileStr := strings.TrimSuffix(string(fileArr), "\n")
+
+	var answer string
+	if args[1] == "1" {
+		answer = part1(fileStr)
+	} else {
+		answer = part2(fileStr)
+	}
+	fmt.Println(answer)
+
+}
+
+func part1(fileTxt string) string {
+	var total, largest int
+	row := strings.SplitSeq(fileTxt, "\n")
+	for batteries := range row {
+		for i, battery1 := range batteries {
+			for _, battery2 := range batteries[i+1:] {
+				num, _ := strconv.Atoi(string([]rune{battery1, battery2}))
+				if num > largest {
+					largest = num
+				}
+			}
+		}
+		total += largest
+		largest = 0
+	}
+	return strconv.Itoa(total)
+}
+
+func part2(fileTxt string) string {
+
+	var total int
+	row := strings.SplitSeq(fileTxt, "\n")
+	for batteries := range row {
+		total += findLargest(batteries, 12)
+
+	}
+	return strconv.Itoa(total)
+}
+
+func findLargest(amount string, num int) {
+	var largest int
+	finalIdx := len(amount) - num
+	for i, test := range amount[:finalIdx] {
+		nextSequence := findLargest(amount[i:], num - 1)
+		testAmount, err :=string(test)	
+	
+	
+	
+	
+
+	 
+																		
+
+		
+		
+
+
+	return largest
+}
